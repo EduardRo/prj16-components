acd9dbb354360f4c242d71259bed70021a6b277e
EduardRo <eduardstancu@yahoo.com>
2021-03-16 16:10:47 +0200

    add component

---
+++ resources/views/components/cmp-frst.blade.php
@@ -0,0 +1,7 @@
+<div>
+    Waste no more time arguing what a good man should be, be one. - Marcus Aurelius
+    <div>{{$name}}</div>
+    <div>{{$message}}</div>
+    <button>Ok</button>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\View\Components\cmpFrst;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function(){
+    return view('home');
+});
+
+Route::get('/component', function(){
+    return view('components.cmp-frst');
 });
